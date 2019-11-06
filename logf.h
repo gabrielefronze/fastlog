cf94a05c633ea28476eb0d672ba398b3a25ef4ce
Gabriele <sucre.91@hotmail.it>
2019-11-06 16:01:02 +0100

    Initial commit

---
+++ logf.h
@@ -0,0 +1,56 @@
+//
+// Created by Gabriele Gaetano Fronzé on 2019-11-06.
+//
+
+#ifndef LOGF_LOGF_H
+#define LOGF_LOGF_H
+
+#include <iostream>
+#include <string_view>
+#include <array>
+
+enum Level{
+  SILENT = 0,
+  ERROR = 1,
+  INFO = 2,
+  DEBUG = 3
+} typedef Level;
+
+static Level logLevel = INFO;
+
+static constexpr std::array Colors
+{
+    "",
+    "\x1B[1;31m", // ERROR: Bold Red
+    "\x1B[1;34m", // INFO: Bold Blue
+    "\x1B[1;33m"  // DEBUG: Green
+};
+
+void logf_internal(const Level level, const std::string s)
+{
+  std::FILE* output = (level == Level::ERROR ? stderr : stdout);
+  if (level <= logLevel)
+  {
+    std::fprintf(output, "%s", Colors[level]);
+    std::fprintf(output, "%s", std::data(s));
+    std::fprintf(output, "\x1B[0m");
+    std::fprintf(output, "\n");
+  }
+}
+
+template<typename... Args>
+void logf_internal(const Level level, const std::string_view s, Args... args)
+{
+    std::FILE* output = (level == Level::ERROR ? stderr : stdout);
+    if (level <= logLevel)
+    {
+        std::fprintf(output, "%s", Colors[level]);
+        std::fprintf(output, std::data(s), args...);
+        std::fprintf(output, "\x1B[0m");
+        std::fprintf(output, "\n");
+    }
+}
+
+#define logf(lvl, msg, ...) { logf_internal(lvl, "" msg, ##__VA_ARGS__); } (void)0
+
+#endif //LOGF_LOGF_H
---
+++ main.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Gabriele Gaetano Fronzé on 2019-11-06.
+//
+
+#include "logf.h"
+
+int main(){
+  std::cout << "Showing effect of various loglevels:";
+
+  std::cout << "\n\nLOGLEVEL = DEBUG\n";
+
+  logLevel = DEBUG;
+
+  logf(INFO, "Info");
+  logf(DEBUG, "Debug");
+  logf(ERROR, "Error");
+
+  std::cout << "\n\nLOGLEVEL = INFO\n";
+
+  logLevel = INFO;
+
+  logf(INFO, "Info");
+  logf(DEBUG, "Debug");
+  logf(ERROR, "Error");
+
+  std::cout << "\n\nLOGLEVEL = ERROR\n";
+
+  logLevel = ERROR;
+
+  logf(INFO, "Info");
+  logf(DEBUG, "Debug");
+  logf(ERROR, "Error");
+
+  std::cout << "\n\nLOGLEVEL = SILENT\n";
+
+  logLevel = SILENT;
+
+  logf(INFO, "Info");
+  logf(DEBUG, "Debug");
+  logf(ERROR, "Error");
+
+  return 0;
+}
